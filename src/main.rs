8d70ee1cc770590a186399c250ee49ad8be32788
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-29 20:21:48 -0500

    variants of using enum2

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,26 @@
 fn main() {
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
     }
-
-    // let home = IpAddr::V4("127,0,0,1");
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    /*  
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-    */
-
-    /* 
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
     
-    fn route(ip_kind: IpAddrKind) { }
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
-    */
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+    
+    impl Message {
+        fn call(&self) {
+        // method body would be defined here
+        }
+        }
+        let m = Message::Write(String::from("hello"));
+        m.call();
 }
