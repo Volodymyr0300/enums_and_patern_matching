af6584febb188cdf51b18a6cb34b58d213df2ee5
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-05-04 19:37:35 -0500

    option_enum

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,24 @@ fn main() {
         }
         let m = Message::Write(String::from("hello"));
         m.call();
+    
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+
+    /* 
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    */
+
+    /* 
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    let sum = x + y;
+    */
+
+    
+
 }
