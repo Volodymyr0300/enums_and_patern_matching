6fa6f210b896abeed202b3c561c24928d4016ed0
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-05-30 08:49:15 -0500

    the placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    fn plus_one(x: Option<i32>) -> Option<i32> {
-            match x {
-                None => None,
-                Some(i) => Some(i + 1),
-            }
-        }
-
-        let five = Some(5);
-        let _six = plus_one(five);
-        let _none = plus_one(None);
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
