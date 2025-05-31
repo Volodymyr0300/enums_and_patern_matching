3971efad54735050587c1e564daae7711c271762
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-05-31 09:44:08 -0500

    control_flow_if_let

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::char;
+
 fn main() {
-    let some_u8_value = 0u8;
+    let some_u8_value = Some(0u8);
+    
     match some_u8_value {
-        1 => println!("one"),
-        3 => println!("three"),
-        5 => println!("five"),
-        7 => println!("seven"),
+        Some(3) => println!("three"),
         _ => (),
     }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let tup (u8, i32, f64, string, char, array, tup) = (0u8, 0i32, 0f64, String::new(), 'a', [0u8; 10], (0u8, 0i32));
+    println!("tup: {:?}", tup);
 }
